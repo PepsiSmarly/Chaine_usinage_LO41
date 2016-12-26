ff215c84e26e3eea2186eca347337a677b59484f
Thomas Gredin <t.gredin@gmx.fr>
2016-12-26 16:50:42 +0100

    Implémentation des méthodes pour la création/destruction des arguments Robot_Args

--- ThreadMethods.c
+++ ThreadMethods.c
@@ -4,6 +4,7 @@ void* factoryTable_Thread(void* args)
 {
     FactoryTable_Args* arg = (FactoryTable_Args*)args;
 
+    pthread_exit(0);
 }
 
 FactoryTable_Args* factoryTable_Args_Create(
@@ -23,5 +24,24 @@ void factoryTable_Args_Destroy(FactoryTable_Args* _ft_Args)
 {
     if(_ft_Args == NULL)
         return;
+        
     free(_ft_Args);
+}
+
+Robot_Args* robot_Args_Create(Robot* _robot, Network* _network)
+{
+    Robot_Args* ra = malloc(sizeof(Robot_Args));
+
+    ra->robot = _robot;
+    ra->network = _network;
+
+    return ra;
+}
+
+void robot_Args_Destroy(Robot_Args* _robot_Args)
+{
+    if(_robot_Args == NULL)
+        return;
+
+    free(_robot_Args);
 }
